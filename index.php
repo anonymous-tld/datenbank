377afb08a0bc4fd8ee54348bcf87d2714971e01a
Anonymous <44589447+anonymous-tld@users.noreply.github.com>
2020-10-16 14:19:56 +0200

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<title>Demo-Termine/Aktueller Tag</title>
-[<b><a href=./>Tagesliste</a></b>] [<a href=warteschlange.php>Warteschlange</a>] [<a href=sperrliste.php>Sperrliste</a>] [<a href=neuetermine.php>Alle neuen Termine</a>] [<a href=datenbestand.php>Datenbestand</a>]<br><br>
-<b>Demo-Termine</b> von https://terminkalender.top <br><br>
-<i>Zum Redaktionsschluss um
-13:50 Uhr, wurden f&uuml;r den heutigen Tag folgende Demotermine gemeldet:</i><br><br>
-&#128308; Freitag, 16. Oktober 2020<br>
-&#9654; DE, Göppingen, EWS Arena (Kundgebung) &#128337; 17:30<br>&#9654; DE, Lemgo, Marktplatz (Kundgebung) &#128337; 16:00<br>&#9654; DE, Sinsheim, Burgplatz (Kundgebung) &#128337; 17:00<br>&#9654; DE, Speyer, Dom (Spaziergang) &#128337; 17:00<br>
