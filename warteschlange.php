ba59ce3eab215218733169d35fbdf4233d4c224a
anonymous-tld <info@anonymous.support>
2020-10-16 10:59:52 +0000

    Update warteschlange.php from https://demo.terminkalender.top/tag/warteschlange.php

---
+++ warteschlange.php
@@ -0,0 +1,8 @@
+<title>Demo-Termine/Status</title>
+[<a href=./>Tagesliste</a>] [<b><a href=warteschlange.php>Warteschlange</a></b>] [<a href=sperrliste.php>Sperrliste</a>] [<a href=neuetermine.php>Alle neuen Termine</a>] [<a href=datenbestand.php>Datenbestand</a>]<br><br>
+<b>Demo-Termine</b> von https://terminkalender.top <br><br>
+<i>Mit dem Stand um
+12:59:52 Uhr, warten folgende Demotermine auf die Freischaltung:</i><br><br>
+
+
+
