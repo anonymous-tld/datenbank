f3a7a4f33fd46c2c8dbf937fcb1ff2c6c6d98d78
anonymous-tld <info@anonymous.support>
2020-10-16 11:22:36 +0000

    Update warteschlange.php from https://demo.terminkalender.top/tag/warteschlange.php

--- warteschlange.php
+++ warteschlange.php
@@ -2,7 +2,8 @@
 [<a href=./>Tagesliste</a>] [<b><a href=warteschlange.php>Warteschlange</a></b>] [<a href=sperrliste.php>Sperrliste</a>] [<a href=neuetermine.php>Alle neuen Termine</a>] [<a href=datenbestand.php>Datenbestand</a>]<br><br>
 <b>Demo-Termine</b> von https://terminkalender.top <br><br>
 <i>Mit dem Stand um
-12:59:52 Uhr, warten folgende Demotermine auf die Freischaltung:</i><br><br>
+13:22:36 Uhr, warten folgende Demotermine auf die Freischaltung:</i><br><br>
 
 
 
+&#9654; 2020-10-18, DE, Nürnberg, Kornmarkt (Kundgebung) &#128337; 14:00<br>
